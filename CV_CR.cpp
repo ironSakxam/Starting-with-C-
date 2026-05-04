da352fe2e807940cf1099941d12e7619dff74899
sakshammandal12345-ui <sakshammandal12345@gmail.com>
2026-05-04 16:23:13 +0545

    UPDATE : fix some errors

--- CV_CR.cpp
+++ CV_CR.cpp
@@ -1,18 +1,17 @@
 #include<iostream>
 using namespace std ;
 class student{
-    
-    public :                                               //call by value = copy pass -. no change in original value
-                                                            // & call by reference 
-    int marks ;
-};
+    public :                                               //call by value = copy pass -> no change in original value                                                            // & call by reference -> change in original value
+    int marks ;                                              //& call by reference -> change in original value 
+};                                                                           
 void displayUpdated(student s) {
     s.marks = 100 ;
 }
+
 int main(){
     student s1 ;
-    s1.marks = 50 ;                      // 100???
-                                           //50??
+    s1.marks = 50 ;                      // 100??? 
+                                           //50???
     displayUpdated(s1);
     cout << s1.marks << endl ;
 }
