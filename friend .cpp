c894f5bd4b44c28f73b1aed44ae11bb5005fa920
sakshammandal12345-ui <sakshammandal12345@gmail.com>
2026-05-12 12:25:37 +0545

    UPDATE :Add files/Friend_class

--- friend .cpp
+++ friend .cpp
@@ -1,8 +1,33 @@
 #include<iostream>
 using namespace std ;
-class Area ;
-class Rectangle{
-    private :
-    int l , b ;
-    cout << "Area is " 
-}
+class Sample {
+    int code ;
+    static int count ; 
+                       public :
+                       void set_code(){
+                         code = ++count  ;
+                    }
+                        void show_code() {
+                          cout << "object number" <<code << endl ;
+                    } 
+                      static void display_count() {
+                        cout << "count :" <<count << endl ;
+                    }
+};
+
+    int Sample::count ;   
+
+    int main() {
+    Sample obj1 , obj2 ;
+    obj1.set_code() ;
+    obj2.set_code() ;
+                Sample::display_count ;
+          Sample obj3 ;
+          obj3.set_code() ;
+          Sample::display_count ;
+         
+          obj1.show_code() ;
+          obj2.show_code() ;
+          obj3.show_code() ;
+    return 0 ;
+   } 
