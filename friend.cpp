1a9cb9974241f0a308247e2015985c13e67f9b68
sakshammandal12345-ui <sakshammandal12345@gmail.com>
2026-05-12 12:41:34 +0545

    add files/friend_class

---
+++ friend.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std ;
+class Box {
+    double width ;
+    public :
+    friend void printWidth(Box &box) ;
+    void set_Width(double wid) ;
+} ;
+
+void Box::set_Width(double wid)
+{
+    width = wid ;
+}
+void printWidth(Box &box) {
+    cout << "Width of the box = " <<box.width <<endl ;
+}
+
+int main() {
+    Box box ;
+    box.set_Width(10.0) ;
+    printWidth (box) ;
+return 0 ;
+}
+
+
